f2d1d284fb2a1e6c82f2b1d173c75cead960a003
David Raygoza Gómez <raygoza.gomez@gmail.com>
2019-04-04 17:43:58 -0600

    v 0.8
    * La clase de consulta a Base de datos ya acepta argumentos
    * La consulta se hace al presionar el boton, no al arrancar

--- app/src/main/java/com/example/accesomysql/MainActivity.java
+++ app/src/main/java/com/example/accesomysql/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.accesomysql.accesoDB.AccesoDB;
 
@@ -17,19 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Thread hilo = new Thread(accesoDB);
+    }
 
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, BaseDatosActivity.class);
+
+        Thread hilo = new Thread(accesoDB);
         hilo.start();
         try {
             hilo.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-    }
-
-    public void sendMessage(View view){
-        Intent intent = new Intent(this, BaseDatosActivity.class);
 
         if(accesoDB.getError().compareTo("") == 0) {
             intent.putExtra(RESULTADO_CONSULTA, accesoDB.getVersion());
--- app/src/main/java/com/example/accesomysql/accesoDB/AccesoDB.java
+++ app/src/main/java/com/example/accesomysql/accesoDB/AccesoDB.java
@@ -11,12 +11,31 @@ public class AccesoDB implements Runnable {
     private String version = "-1";
     private String error = "";
 
+    private String url;
+    private String usuario;
+    private String password;
+
+    public AccesoDB() {
+        url = "192.168.100.240";
+        usuario = "usuarioPruebas";
+        password = "qwerty";
+    }
+
+    public AccesoDB(String urlBaseDatos, String usuarioBAseDatos, String passwordBaseDatos) {
+        url = urlBaseDatos;
+        usuario = usuarioBAseDatos;
+        password = passwordBaseDatos;
+    }
+
     @Override
     public void run() {
         try {
             String sql = "SELECT version()";
             Class.forName("com.mysql.jdbc.Driver");
-            Connection conexion = DriverManager.getConnection("jdbc:mysql://192.168.100.240:3306/test", "usuarioPruebas", "qwerty");
+
+            String cadenaConexion = "jdbc:mysql://" + url + ":3306";
+
+            Connection conexion = DriverManager.getConnection(cadenaConexion, usuario, password);
             Statement consulta = conexion.createStatement();
             ResultSet data = consulta.executeQuery(sql);
             data.next();
