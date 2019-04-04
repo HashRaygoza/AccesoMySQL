4b2f9eb43ec87a499c0382f4a9b931e044d08e91
David Raygoza Gómez <raygoza.gomez@gmail.com>
2019-04-04 14:13:42 -0600

    v 0.4
    Se creo la interfaz grafica

---
+++ app/src/main/java/com/example/accesomysql/BaseDatosActivity.java
@@ -0,0 +1,13 @@
+package com.example.accesomysql;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaseDatosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_datos);
+    }
+}
