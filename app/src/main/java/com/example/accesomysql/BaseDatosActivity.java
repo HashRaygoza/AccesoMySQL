4290ce25945c3c3a0b832e8b6c0fb0e4b962f2c7
David Raygoza Gómez <raygoza.gomez@gmail.com>
2019-04-04 15:06:04 -0600

    v 0.6
    Se implemento una prueba de la funcionalidad de la interfaz, para
    enviar datos desde una Activity a otra.

--- app/src/main/java/com/example/accesomysql/BaseDatosActivity.java
+++ app/src/main/java/com/example/accesomysql/BaseDatosActivity.java
@@ -1,7 +1,9 @@
 package com.example.accesomysql;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BaseDatosActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class BaseDatosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base_datos);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.RESULTADO_CONSULTA);
+
+        TextView versionMysql = findViewById( R.id.textView3 );
+        versionMysql.setText(message);
     }
 }
--- app/src/main/java/com/example/accesomysql/MainActivity.java
+++ app/src/main/java/com/example/accesomysql/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String RESULTADO_CONSULTA = "com.example.accesomysql.RESULTADO_CONSULTA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendMessage(View view){
         Intent intent = new Intent(this, BaseDatosActivity.class);
+
+        intent.putExtra(RESULTADO_CONSULTA, "-1");
+
         startActivity(intent);
     }
 }
